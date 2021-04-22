2d204ac3e592191ec94cd05237af9dca57ac864b
vantoanftu93 <73921998+vantoanftu93@users.noreply.github.com>
2021-04-22 10:35:55 +0700

    add JwtInterceptor

---
+++ src/app/helper/jwt.interceptor.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {AuthenService} from '../service/authenServie/authen.service';
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+
+  constructor(private authenService: AuthenService) {
+  }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const currentUser = this.authenService.currentUserValue;
+    alert(currentUser.accessToken)
+    if (currentUser && currentUser.accessToken) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${currentUser.accessToken}`
+        }
+      });
+    }
+    return next.handle(request);
+  }
+}
