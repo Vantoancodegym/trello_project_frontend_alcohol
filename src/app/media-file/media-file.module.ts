fb8c84bca936c807829ef1fce730c489edb3d454
vantoanftu93 <73921998+vantoanftu93@users.noreply.github.com>
2021-04-26 09:38:37 +0700

    create mediaFile module

---
+++ src/app/media-file/media-file.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MediaFileRoutingModule } from './media-file-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MediaFileRoutingModule
+  ]
+})
+export class MediaFileModule { }
