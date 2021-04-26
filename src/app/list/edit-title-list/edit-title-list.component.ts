02a63884ce119ae58676dd22445ba95224b8cdc3
MungC2010G2 <zhaiduong26z@gmail.com>
2021-04-26 14:56:46 +0700

    create editTitleList method in component.ts

--- src/app/list/edit-title-list/edit-title-list.component.ts
+++ src/app/list/edit-title-list/edit-title-list.component.ts
@@ -1,14 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output, TemplateRef} from '@angular/core';
+import {ListService} from "../../service/listService/list.service";
+import {IList} from "../../interface/i-list";
+import {Router} from "@angular/router";
+import {BsModalRef, BsModalService} from "ngx-bootstrap/modal";
 @Component({
   selector: 'app-edit-title-list',
   templateUrl: './edit-title-list.component.html',
   styleUrls: ['./edit-title-list.component.scss']
 })
 export class EditTitleListComponent implements OnInit {
-
-  constructor() { }
+  // @ts-ignore
+  @Input()
+  id: number = 0;
+  list: IList = {
+    title : "",
+    board: {
+      id: 0
+    }
+  }
+  // @ts-ignore
+  modalRef: BsModalRef;
+  constructor(private modalService: BsModalService,private listService: ListService,private router: Router) { }
 
   ngOnInit(): void {
+    this.listService.findListById(this.id).subscribe( result => {
+      this.list = result;
+    })
+  }
+  @Output()
+  isUpdate = new EventEmitter();
+  editTitleList(){
+    this.listService.editTitleList(this.list, this.id).subscribe(() =>{
+      this.isUpdate.emit(true);
+      this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+      this.router.onSameUrlNavigation = 'reload';
+      // @ts-ignore
+      this.router.navigateByUrl("/board/" + this.list?.board?.id) ;
+    });
   }
 
 }
