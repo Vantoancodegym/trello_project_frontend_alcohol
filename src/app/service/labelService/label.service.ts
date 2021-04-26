0af4f069e62eae9f4c4c5f13ec3a83b00184a397
vantoanftu93 <73921998+vantoanftu93@users.noreply.github.com>
2021-04-26 22:14:00 +0700

    create labelService

---
+++ src/app/service/labelService/label.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../environments/environment';
+import {Observable} from 'rxjs';
+import {ILabel} from '../../interface/label';
+const URL_BACKEND = environment.api_url
+@Injectable({
+  providedIn: 'root'
+})
+export class LabelService {
+
+  constructor(private httpClient: HttpClient) { }
+  getListSelected(card_id:number): Observable<ILabel[]>{
+    return this.httpClient.get<ILabel[]>(URL_BACKEND+"labels/selected/" + card_id);
+  }
+  getLabelsByCard(card_id:number): Observable<ILabel[]>{
+    return this.httpClient.get<ILabel[]>(URL_BACKEND + "labels/card/"+ card_id);
+  }
+}
