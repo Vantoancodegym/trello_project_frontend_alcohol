4c9a308158c69c6e5700592f2945ed30eaf3bf9c
MungC2010G2 <zhaiduong26z@gmail.com>
2021-04-30 15:09:13 +0700

    xử lý xong search card theo tiêu đề hoặc nội dung

--- src/app/card/card.module.ts
+++ src/app/card/card.module.ts
@@ -1,19 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { CardRoutingModule } from './card-routing.module';
-import { ShowCardByListIdComponent } from './show-card-by-list-id/show-card-by-list-id.component';
+import {CardRoutingModule} from './card-routing.module';
+import {ShowCardByListIdComponent} from './show-card-by-list-id/show-card-by-list-id.component';
 import {DragDropModule} from '@angular/cdk/drag-drop';
 import {BrowserModule} from '@angular/platform-browser';
-import { CreateCardComponent } from './create-card/create-card.component';
+import {CreateCardComponent} from './create-card/create-card.component';
 import {ShowListComponent} from '../list/show-list/show-list.component';
 import {ModalModule} from 'ngx-bootstrap/modal';
 import {FormsModule} from '@angular/forms';
-import { EditCardComponent } from './edit-card/edit-card.component';
-import { DetailCardComponent } from './detail-card/detail-card.component';
-import { SearchCardComponent } from './search-card/search-card.component';
-
-
+import {EditCardComponent} from './edit-card/edit-card.component';
+import {DetailCardComponent} from './detail-card/detail-card.component';
+import {SearchCardComponent} from './search-card/search-card.component';
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatOptionModule} from "@angular/material/core";
+import {Ng2OrderModule} from "ng2-order-pipe";
+import {Ng2SearchPipeModule} from "ng2-search-filter";
+import {NgxPaginationModule} from "ngx-pagination";
 @NgModule({
   declarations: [
     ShowCardByListIdComponent,
@@ -32,9 +35,17 @@ import { SearchCardComponent } from './search-card/search-card.component';
     DragDropModule,
     BrowserModule,
     ModalModule,
-    FormsModule
-  ],
+    FormsModule,
+    Ng2OrderModule,
+    Ng2SearchPipeModule,
+    NgxPaginationModule,
+    MatCheckboxModule,
+    MatOptionModule,],
   bootstrap: [ShowListComponent]
 
 })
-export class CardModule { }
+export class CardModule {
+}
+
+
+
--- src/app/card/search-card/search-card.component.ts
+++ src/app/card/search-card/search-card.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {CardService} from "../../service/cardService/card.service";
+import {ICard} from "../../interface/i-card";
 
 @Component({
   selector: 'app-search-card',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-card.component.scss']
 })
 export class SearchCardComponent implements OnInit {
-
-  constructor() { }
+  input: string = "";
+  input1: string = "";
+  list: ICard[] = [];
+  constructor(private cardService:CardService) { }
 
   ngOnInit(): void {
+    this.cardService.showAllCard().subscribe(result =>{
+      this.list = result;
+    })
   }
-
+  searchCardByContentOrTitle(){
+    if(this.input == ""){
+      this.ngOnInit();
+    } else {
+      this.list = this.list.filter(res => {
+        return res.title?.toLocaleLowerCase().match(this.input.toLocaleLowerCase())
+          || res.content?.toLocaleLowerCase().match(this.input.toLocaleLowerCase());
+      })
+    }
+  }
 }
