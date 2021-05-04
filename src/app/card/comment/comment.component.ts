bbec8c05fa4f189b050b090af9f6e8a443232019
MungC2010G2 <zhaiduong26z@gmail.com>
2021-05-04 21:00:11 +0700

    create method createComment

--- src/app/card/comment/comment.component.ts
+++ src/app/card/comment/comment.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {CommentService} from '../../service/commentService/comment.service';
+import {IComment} from '../../interface/i-comment';
 
 @Component({
   selector: 'app-comment',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comment.component.scss']
 })
 export class CommentComponent implements OnInit {
-
-  constructor() { }
+  comment: IComment = {};
+  constructor(private commentService:CommentService) { }
 
   ngOnInit(): void {
+  }
+  createComment(){
+    this.commentService.createComment(this.comment).subscribe(()=>{
+      console.log("tạo mới thành công");
+    })
   }
 
 }
