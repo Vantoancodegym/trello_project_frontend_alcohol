f20d7d7290afbec21807b5cdb79dc61e99425373
vantoanftu93 <73921998+vantoanftu93@users.noreply.github.com>
2021-05-04 14:06:24 +0700

    create INotification modal

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, TemplateRef} from '@angular/core';
 import {AuthenService} from '../service/authenServie/authen.service';
+import {INotification} from '../interface/i-notification';
+import {BsModalRef, BsModalService} from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-header',
@@ -7,10 +9,18 @@ import {AuthenService} from '../service/authenServie/authen.service';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor(public authenService: AuthenService) { }
+  notifications: INotification[] = [];
+  // @ts-ignore
+  modalRef: BsModalRef;
+  constructor(public authenService: AuthenService, private modalService: BsModalService) { }
 
   ngOnInit(): void {
+  }
+  openModalWithClass(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(
+      template,
+      Object.assign({}, { class: 'center modal-lg' })
+    );
   }
 
 }
