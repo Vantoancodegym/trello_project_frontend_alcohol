111c85f27daaff258b9f8ba51ddd150adb966922
MungC2010G2 <zhaiduong26z@gmail.com>
2021-05-04 20:55:40 +0700

    create commentService and method createComment

---
+++ src/app/service/commentService/comment.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {environment} from '../../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {IComment} from '../../interface/i-comment';
+import {Observable} from 'rxjs';
+const API_BACKEND = environment
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+  constructor(private httpClient: HttpClient) { }
+
+  createComment(comment:IComment):Observable<any>{
+   return  this.httpClient.post(API_BACKEND+"comments/create",comment);
+  }
+}
